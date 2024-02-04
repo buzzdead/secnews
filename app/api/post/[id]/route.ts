334e4fb580a175d19e1d4e706013698fa092fd02
buzzdead <sigmundvolden@gmail.com>
2024-02-04 15:36:06 +0100

    .

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -1,8 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { NextApiRequest } from "next/types";
 
-export async function DELETE(request: NextApiRequest, {params}: {params: Record<string, string | string[]>}){
+export async function DELETE(request: NextRequest | Request, {params}: {params: Record<string, string | string[]>}){
     const id = params.id as string;
     
     const post = await prisma.post.delete({
