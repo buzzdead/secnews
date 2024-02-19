1b96fd1b7c6d1313fea751aad155d9ac54b1d027
buzzdead <sigmundvolden@gmail.com>
2024-02-19 20:19:12 +0100

    merge conflcit

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -1,13 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-<<<<<<< HEAD
-export async function DELETE(request: NextRequest | Request, {params}: {params: Record<string, string | string[]>}){
-    const id = params.id as string;
-=======
 export async function DELETE(request: NextRequest | Request, {params}: any){
     const id = params.id;
->>>>>>> tailwind
     
     const post = await prisma.post.delete({
         where: {id}
