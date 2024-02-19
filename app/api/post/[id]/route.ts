152d4f36a03398f372b602243e0f3f3398905553
buzzdead <sigmundvolden@gmail.com>
2024-02-19 20:08:46 +0100

    change

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -1,8 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { NextApiRequest } from "next/types";
 
-export async function DELETE(request: NextApiRequest, {params}: any){
+export async function DELETE(request: NextRequest | Request, {params}: any){
     const id = params.id;
     
     const post = await prisma.post.delete({
